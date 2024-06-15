6293bab033bd98f98b741c2599b13f49c383a4fd
CESCO <cescovalledev@gmail.com>
2024-06-15 15:23:24 -0600

    chapter 4

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return <p>Invoices Page</p>;
+};
+
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return <p>Dashboard Page</p>;
+};
+
+export default Page;
